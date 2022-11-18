44de2aab6b0ea95e355aab405e74055d40864f9f
ovie009 <ovieiffieu2much@gmail.com>
2022-11-18 11:18:09 +0100

    introduction to variables and concantenation

--- index.php
+++ index.php
@@ -7,9 +7,27 @@
     <title>PHP Review</title>
 </head>
 <body>
+    <!-- form with no action attribute -->
+    <!-- which means the data would be submitted to the same page -->
+    <form method="GET">
+        <input type="text" name="user" id="" placeholder="your name">
+        <button type="submit">SUBMIT</button>
+    </form>
     <?php
-        // outputing information on a browser
-        echo "Hello world";
+        // variables
+
+        // string
+        $name = "John Doe";
+        echo $name;
+
+        // concantenation
+        echo '<br>'; // new line
+        echo $name.' is a handsome fellow';
+        
+        echo '<br>'; // new line
+        $user =  $_GET["user"]; // get user variable submitted from input using GET method
+        echo $user.' is a logged in'; // concantenate variable with text
+
     ?>
 </body>
 </html>
