f194677d49e86640946073fee5490bdb444b6557
ovie009 <ovieiffieu2much@gmail.com>
2022-11-18 11:06:47 +0100

    outputing information on a browser

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<?php
-    echo "Hello world"
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Review</title>
+</head>
+<body>
+    <?php
+        // outputing information on a browser
+        echo "Hello world";
+    ?>
+</body>
+</html>
