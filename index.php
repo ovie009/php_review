3f2d54d86e199e5ba636530495b48bc431305890
ovie009 <ovieiffieu2much@gmail.com>
2022-11-19 18:00:41 +0100

    xor and other logical operator

--- index.php
+++ index.php
@@ -8,62 +8,39 @@
 </head>
 <body>
     <?php
-        // operators
-
-        // Addition
-        echo "<strong>Addition</strong><br>";
-        echo "10+5=";
-        echo 10+5;
-        
-        // substraction
-        echo "<br><strong>Substraction</strong><br>";
-        echo "10-5=";
-        echo 10-5;
-        
-        // division
-        echo "<br><strong>Division</strong><br>";
-        echo "10/5=";
-        echo 10/5;
-        
-        // multiplication
-        echo "<br><strong>Multiplication</strong><br>";
-        echo "10*5=";
-        echo 10*5;
-
-        // modulus
-        echo "<br><strong>Modulus</strong><br>";
-        echo "10%5=";
-        echo 10%5;
-        
-        // power
-        echo "<br><strong>Power</strong><br>";
-        echo "10<sup>5</sup>=";
-        echo 10**5;
-        
-        // increment operator
+        // Logical operators
         $x = 10;
-        echo "<br><strong>Increment</strong><br>";
-        echo 'if x = 10, x++ = ';
-        $x++;
-        echo $x;
-        echo "<br><strong>Increment by a value</strong><br>";
-        $y = 27;
-        echo 'if y = '.$y.', y += 9 is equal to ';
-        $y+=9;
-        echo $y;
+        $y = 20;
 
-        // Decrement operator
-        $x = 10;
-        echo "<br><strong>Decrement</strong><br>";
-        echo 'if x = 10, x-- = ';
-        $x--;
-        echo $x;
-        echo "<br><strong>Decrement by a value</strong><br>";
-        $y = 27;
-        echo 'if y = '.$y.', y -= 9 is equal to ';
-        $y-=9;
-        echo $y;
+        // or
+        if ($x == $y or 1 == 1) {
+            echo 'True';
+            echo '<br>';
+        }
+        
+        // and
+        if ($x == $y and 1 == 1) {
+            echo 'True';
+            echo '<br>';
+        }
 
+        // or logical operator
+        if ($x == $y || 1 == 1) {
+            echo 'True';
+            echo '<br>';
+        }
+
+        // and logical operator
+        if ($x != $y && 1 != 2) {
+            echo 'True';
+            echo '<br>';
+        }
+
+        // xor (xclusive or, only runs if any of the conditions are true, not both)
+        if ($x != $y xor 1 == 2) {
+            echo 'True';
+            echo '<br>';
+        }
     ?>
 </body>
 </html>
