3606ec73d6f9f09213cfe97a17c1f07227465c5a
ovie009 <ovieiffieu2much@gmail.com>
2022-11-19 18:36:07 +0100

    basic calculator done

--- index.php
+++ index.php
@@ -7,58 +7,62 @@
     <title>PHP Review</title>
 </head>
 <body>
-    <?php
 
-        // conditional statements
-        $age = 10;
-        echo 'John Doe is '.$age.'years old, <strong><em>John is ';
+    <form action="index.php" method="get">
+        <input type="number" name="num1" placeholder="number 1" required>
+        <input type="number" name="num2" placeholder="number 2" required>
+        <br>
+        <select name="operation">
+            <option value="add">add</option>
+            <option value="sub">substract</option>
+            <option value="mul">multiply</option>
+            <option value="div">divide</option>
+        </select>
+        <button type="submit" name="submit">calculate</button>
+    </form>
 
-        switch ($age) {
-            case $age < 13:
-                # code...
-                echo 'a child';
-                break;
-                
-                case $age < 20:
-                # code...
-                echo 'a teenager';
-                break;
-                
-                case $age < 30:
-                # code...
-                echo 'in his 20\'s';
-                break;
-            
-            case $age < 40:
-                # code...
-                echo 'in his 30\'s';
-                break;
-            
-            case $age < 50:
-                # code...
-                echo 'in his 40\'s';
-                break;
-            
-            case $age < 60:
-                # code...
-                echo 'in his 50\'s';
-                break;
-            
-            case $age < 70:
-                # code...
-                echo 'in his 60\'s';
-                break;
-            
-            default:
-                # code...
-                echo 'very old';
-                break;
-        }
-            
-        echo '</em></strong>'
-        
+    <p> Answer: 
+        <strong>
+            <?php
+                $x = 0;
+                if (isset($_GET['submit'])) {
+                    # code...
+                    echo $_GET['num1'];
+                    switch ($_GET['operation']) {
 
-        
-    ?>
+                        case 'sub':
+                            # code...
+                            // substract numbers
+                            $x = $_GET['num1'] - $_GET['num2'];
+                            echo '-';
+                            break;
+                            
+                        case 'mul':
+                            # code...
+                            // multiply numbers
+                            $x = $_GET['num1'] * $_GET['num2'];
+                            echo '*';
+                            break;
+                            
+                        case 'div':
+                            # code...
+                            // divide numbers
+                            $x = $_GET['num1'] / $_GET['num2'];
+                            echo '/';
+                            break;
+                            
+                        default:
+                            # code...
+                            // default action, add numbers
+                            $x = $_GET['num1'] + $_GET['num2'];
+                            echo '+';
+                            break;
+                    }
+
+                    echo $_GET['num2'].' = '.$x;
+                }
+            ?>
+        </strong>
+    </p>
 </body>
 </html>
